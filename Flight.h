5cd08a0cda5c0d9662b5b261a15127b18d18949b
Steven <steven.wu@ucalgary.ca>
2025-11-24 11:45:48 -0700

    flight and passenger .h

---
+++ Flight.h
@@ -0,0 +1,45 @@
+#ifndef FLIGHT_H
+#define FLIGHT_H
+
+#include <string>
+#include <vector>
+#include "Passenger.h"
+using namespace std;
+
+class flight {
+private:
+    string flightid;        // id
+    string departure;       // departure city
+    string destination;     // destination city
+    int rows;               // total rows
+    int seatsperrow;        // seats per row
+    vector<passenger> passengers; // vector of all passengers
+
+public:
+    flight();
+    flight(string id, string dep, string des, int r, int spr);
+
+    // read flight info
+    string getflightid() const;
+    string getdeparture() const;
+    string getdestination() const;
+    int getrows() const;
+    int getseatsperrow() const;
+
+    // add, remove, and check seat
+    void addpassenger(const passenger& p);
+    bool removepassenger(int passengerid);
+    bool isseattaken(int row, char seat) const;
+
+    // display 
+    void displayseatmap() const;
+    void displaypassengerlist() const;
+
+    // save file
+    void savepassengerstofile(ofstream& fout) const;
+
+    // get list (for loading)
+    vector<passenger>& getpassengerlist();
+};
+
+#endif
---
+++ Passenger.h
@@ -0,0 +1,34 @@
+#ifndef PASSENGER_H
+#define PASSENGER_H
+#include <string>
+using namespace std;
+
+class passenger {
+private:
+    string firstname;
+    string lastname;
+    string phonenum;
+    int row;        
+    char seat;      
+    int id;         
+
+public:
+    passenger();
+    passenger(string f, string l, string p, int r, char s, int pid);
+            //first , last , phonenum , row , seat , id
+    string getfirstname() const;
+    string getlastname() const;
+    string getphone() const;
+    int getrow() const;
+    char getseat() const;
+    int getid() const;
+
+    void setfirstname(const string& f);
+    void setlastname(const string& l);
+    void setphone(const string& p);
+    void setrow(int r);
+    void setseat(char s);
+    void setid(int pid);
+};
+
+#endif

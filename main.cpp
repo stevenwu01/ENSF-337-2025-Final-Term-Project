80514a39da85b1cfd796582198463e1ed10b84b2
Steven <steven.wu@ucalgary.ca>
2025-11-30 13:48:28 -0700

    Add main.cpp initial structure

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Flight.h"
+#include "Passenger.h"
+
+using namespace std;
+
+void loadflights(vector<flight>& flights);
+void loadpassengers(vector<flight>& flights);
+void saveall(const vector<flight>& flights);
+void showmenu();
+
+
+int main(){
+    //print welcome 
+    //wait for user
+    //create menu
+
+    //1 show all flight and let user choose
+    //2 set map
+    //3 passenger list
+    //4 add passenger
+    //5 remove passenger
+    //6 save to file
+    //print exit massage
+    return 0;
+}
+
+
+void loadflights(vector<flight>& flights){
+
+}
+void loadpassengers(vector<flight>& flights){
+
+}
+void saveall(const vector<flight>& flights){
+
+}
+void showmenu(){
+    
+}

2985586d5e0bb7b8ab740666f158865a0302032d
Steven <steven.wu@ucalgary.ca>
2025-11-30 19:23:06 -0700

    Update main.cpp with welcome + menu functions

--- main.cpp
+++ main.cpp
@@ -10,20 +10,16 @@ void loadflights(vector<flight>& flights);
 void loadpassengers(vector<flight>& flights);
 void saveall(const vector<flight>& flights);
 void showmenu();
-
+void printwelcome();
 
 int main(){
+    printwelcome();
+    showmenu();
     //print welcome 
     //wait for user
     //create menu
 
-    //1 show all flight and let user choose
-    //2 set map
-    //3 passenger list
-    //4 add passenger
-    //5 remove passenger
-    //6 save to file
-    //print exit massage
+    
     return 0;
 }
 
@@ -38,5 +34,27 @@ void saveall(const vector<flight>& flights){
 
 }
 void showmenu(){
-    
+    //1 show all flight and let user choose
+    //2 set map
+    //3 passenger list
+    //4 add passenger
+    //5 remove passenger
+    //6 save to file
+    //print exit massage
+    cout << "\nPlease select one the following options:\n\n";
+    cout << "  1. Select a flight\n";
+    cout << "  2. Display Flight Seat Map.\n";
+    cout << "  3. Display Passengers Information.\n";
+    cout << "  4. Add a New Passenger.\n";
+    cout << "  5. Remove an Existing Passenger\n";
+    cout << "  6. Save data\n";
+    cout << "  7. Quit.\n\n";
+    cout << "Enter your choice: (1, 2, 3, 4, 5, 6, or 7) ";
+
+}
+void printwelcome(){
+    cout << "Version: 1.0"<<endl;
+    cout << "Term Project - Flight Management Program in C++"<<endl;
+    cout << "Produced by: Student Name"<<endl;
+    cout << "Year: 2023"<<endl;
 }

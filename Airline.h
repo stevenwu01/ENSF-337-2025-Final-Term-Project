3cbbd6436ef86b1eb68dd88481eae80e2f02274e
FiemnaEkpombang <fiem.ekpombangfiemna@ucalgary.ca>
2025-11-30 22:14:33 -0700

    Update Airline.h

--- Airline.h
+++ Airline.h
@@ -15,12 +15,12 @@ public:
     Airline(const std::string& airlineName);
 
     std::string getName() const;
-    std::vector<flight>& getFlights();
-    const std::vector<flight>& getFlights() const;
+    std::vector<Flight>& getFlights();
+    const std::vector<Flight>& getFlights() const;
 
-    void addFlight(const flight& f);
+    void addFlight(const Flight& f);
     int findFlightIndex(const std::string& flightId) const;
-    flight* getFlightById(const std::string& flightId);
+    Flight* getFlightById(const std::string& flightId);
 };
 
 #endif

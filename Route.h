6f7817f0215b2337dd1426ed660300504ee9c6a2
FiemnaEkpombang <fiem.ekpombangfiemna@ucalgary.ca>
2025-12-01 19:35:44 -0700

    Update Route.h

--- Route.h
+++ Route.h
@@ -10,13 +10,16 @@ class route{
         string destination;
 
     public:
+    // Promises to create a route with empty values.
         route();
+        // Promises to create a route with the given source and destination.
         route(string s,string d);
-        string getsource() const;
-        string getdestination()const;
 
-        void setsource(const string&s);
-        void setdestination(const string& d);
+        string getsource() const;  // Promises to return the source location.
+        string getdestination()const;      // Promises to return the destination location.
+
+        void setsource(const string&s);  // Promises to set the source location.
+        void setdestination(const string& d);  // Promises to set the destination location.
         
     };
 
